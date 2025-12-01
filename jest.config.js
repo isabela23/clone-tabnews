4c0f2ed77b9cd6d00cd0a55b5e5375f253be64f9
isabela23 <71663469+isabela23@users.noreply.github.com>
2025-12-01 23:14:35 +0000

    makes 'npm test' more robust

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 };
 
 module.exports = createJestConfig(customJestConfig);
