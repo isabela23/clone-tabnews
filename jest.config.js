ad0c74babec9a582c457629284f7fb16b77c11f4
isabela23 <71663469+isabela23@users.noreply.github.com>
2025-12-04 00:42:22 +0000

    fix eslint linting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv").config({ path: ".env.development" });
+// const dotenv = require("dotenv").config({ path: ".env.development" });
 
 const nextJest = require("next/jest");
 
