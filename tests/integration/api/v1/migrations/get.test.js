eaee43dc00898c54a6994936d2a243da5420542f
isabela23 <71663469+isabela23@users.noreply.github.com>
2025-12-09 20:45:13 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,17 +6,21 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 });
 
-test("Get to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  // Espera que a pagina esteja funcionando, retornando alguma coisa 200=sucesso
-  expect(response.status).toBe(200);
+      // Espera que a pagina esteja funcionando, retornando alguma coisa 200=sucesso
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  // Espera que o corpo da tela migration retorne uma lista
-  expect(Array.isArray(responseBody)).toBe(true);
+      // Espera que o corpo da tela migration retorne uma lista
+      expect(Array.isArray(responseBody)).toBe(true);
 
-  // Espera que o tamanho do corpo seja maior que zero
-  expect(responseBody.length).toBeGreaterThan(0);
+      // Espera que o tamanho do corpo seja maior que zero
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
