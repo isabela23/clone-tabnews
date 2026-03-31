4a170f5f6607c5a6308d50b832f96229a2cd50c4
isabela23 <71663469+isabela23@users.noreply.github.com>
2026-03-31 19:18:23 +0000

    feat: invalidate session cookie on unauthorized error

--- infra/controller.js
+++ infra/controller.js
@@ -14,14 +14,14 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
     return response.status(error.statusCode).json(error);
   }
 
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
+    return response.status(error.statusCode).json(error);
+  }
   const publicErrorObject = new InternalServerError({
     // statusCode: error.statusCode,
     cause: error,
